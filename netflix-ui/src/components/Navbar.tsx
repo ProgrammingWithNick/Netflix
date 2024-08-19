c8004496fa034197e4a953183f16ef3c66224e1c
nickk <nikhilkhavdu441@gmail.com>
2024-08-19 22:15:08 +0530

    second commit

--- netflix-ui/src/components/Navbar.tsx
+++ netflix-ui/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { logoImg } from '../utils/images'
 import { FaPowerOff, FaSearch } from 'react-icons/fa'
@@ -8,12 +8,16 @@ import { firebaseAuth } from '../utils/firebase';
 interface NavItemProps {
     name: string;
     link: string;
-    isScrolled: boolean | string; // Make isScrolled optional if not always needed
+    isScrolled?: boolean; // Make isScrolled optional if not always needed
 }
 
-const NavItem: React.FC<NavItemProps> = ({ name, link, isScrolled }) => {
+const NavItem: React.FC<NavItemProps> = ({ name, link,
+    // isScrolled 
+}) => {
     return (
-        <li key={name} className={isScrolled ? "scrolled" : ""}>
+        <li key={name}
+        // className={isScrolled ? "scrolled" : ""}
+        >
             <Link to={link}>{name}</Link>
         </li>
     );
@@ -31,48 +35,51 @@ const Navbar: React.FC = () => {
         if (!currentUser) navigate("/login");
     })
 
-    const isScrolled = true; // Example value, replace with actual logic
+    // const isScrolled = true; // Example value, replace with actual logic
 
-    const [showSearch, setShowSearch] = useState<boolean>(false)
-    const [inputHover, setInputHover] = useState<boolean>(false)
+    // const [showSearch, setShowSearch] = useState<boolean>(false)
+    // const [inputHover, setInputHover] = useState<boolean>(false)
 
     return (
 
-        <nav className="sticky flex gap-3 text-white bg-black">
-            <div className="flex ">
-                <div className="m-auto mx-3 bg-purple-400">
+        <nav className="absolute top-0 z-10 flex w-screen gap-5 text-white backdrop-blur-md">
+            <div className="flex">
+                <div className="items-center justify-center m-auto mx-3 ">
                     <img className="h-20 " src={logoImg} alt="logo" />
                 </div>
-                <ul className="flex gap-5 bg-slate-700">
+                <ul className="flex items-center justify-center gap-5 ">
                     {links.map(({ name, link }) => (
-                        <NavItem key={name} name={name} link={link} isScrolled={isScrolled} />
+                        <NavItem key={name} name={name} link={link}
+                        // isScrolled={isScrolled} 
+                        />
                     ))}
                 </ul>
             </div>
-            <div className="relative flex gap-3 px-8 ml-auto bg-red-400 ">
-                <div className={`search ${showSearch ? "bg-red-700" : ""}`}>
-                    <button
-                        className='bg-zinc-500'
-                        onFocus={() => setShowSearch(true)}
-                        onBlur={() => {
-                            if (!inputHover) setShowSearch(false);
-                        }}>
-                        <FaSearch className="" />
-                    </button>
-                    <input type="text" placeholder='Search'
-                        className='bg-yellow-600 '
-                        onMouseEnter={() => setInputHover(true)}
-                        onMouseLeave={() => setInputHover(false)}
-                        onBlur={() => {
-                            setShowSearch(false);
-                            setInputHover(true);
-                        }}
-                    />
-                </div>
-                <button onClick={() => signOut(firebaseAuth)}
-                    className='bg-yellow-300 mb-14 '
+            <div className="relative flex flex-row-reverse items-center justify-center gap-3 px-8 ml-auto text-white">
+                {/* <div className={`search ${showSearch ? "bg-red-700" : ""}`}> */}
+                <button
+                    className='items-center justify-center '
+                // onFocus={() => setShowSearch(true)}
+                // onBlur={() => {
+                //     if (!inputHover) setShowSearch(false);
+                // }}
                 >
-                    <FaPowerOff />
+                    <FaSearch className="h-5 w-7" />
+                </button>
+                <input type="text" placeholder='Search'
+                    className='w-auto mx-4 text-black cursor-pointer'
+                // onMouseEnter={() => setInputHover(true)}
+                // onMouseLeave={() => setInputHover(false)}
+                // onBlur={() => {
+                //     setShowSearch(false);
+                //     setInputHover(true);
+                // }}
+                />
+                {/* </div> */}
+                <button onClick={() => signOut(firebaseAuth)}
+                    className='items-center justify-center '
+                >
+                    <FaPowerOff className='w-5 h-5 ' />
                 </button>
             </div>
         </nav>
--- netflix-ui/src/pages/Netflix.tsx
+++ netflix-ui/src/pages/Netflix.tsx
@@ -1,19 +1,49 @@
-import { useState } from "react"
+// import { useState } from "react"
 import Navbar from "../components/Navbar";
+import { homeImg, homeTitleImg } from '../utils/images'
+import { FaPlay } from "react-icons/fa"
+import { AiOutlineInfoCircle } from "react-icons/ai"
 
 const Netflix = () => {
-    const [isScrolled, setIsScrolled] = useState(false);
+    // const [isScrolled, setIsScrolled] = useState<boolean>(false);
 
-    window.onload = () => {
-        setIsScrolled(window.pageXOffset === 0 ? false : true);
-        return () => (window.onscroll = null);
-    };
+    // window.onload = () => {
+    //     setIsScrolled(window.pageXOffset === 0 ? false : true);
+    //     return () => (window.onscroll = null);
+    // };
 
     return (
         <div>
-            <Navbar isScrolled={isScrolled} />
+            <Navbar
+            // isScrolled={isScrolled}
+            />
+            <div className="relative bg-black">
+                <img
+                    className="w-screen h-screen"
+                    src={homeImg}
+                    alt="home"
+                />
+                <div className="">
+                    <div className="absolute w-screen h-screen top-40">
+                        <img
+                            className=" backdrop-filter"
+                            src={homeTitleImg}
+                            alt="homeTitle"
+                        />
+                    </div>
+                    <div className="flex flex-row gap-5 ">
+                        <button className="flex items-center justify-center px-2 bg-white">
+                            <FaPlay />Play
+                        </button>
+                        
+                        <button className="flex items-center justify-center px-2 bg-white">
+                            <AiOutlineInfoCircle />More Info
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Netflix
+export default Netflix;
